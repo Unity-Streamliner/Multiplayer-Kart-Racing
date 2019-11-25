cd5ccff2c52c7da0b1d74a42357537d35ad882a6
thisisstreamliner <thisisstreamliner@gmail.com>
2019-11-25 15:39:38 +0200

    Create very simple car Add drive script to car

---
+++ Assets/Drive.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public WheelCollider WheelCollider;
+    public float torque = 200;
+    // Start is called before the first frame update
+    void Start()
+    {
+        WheelCollider = GetComponent<WheelCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float a = Input.GetAxis("Vertical");
+        Go(a);
+    }
+
+    void Go(float accel) 
+    {
+        accel = Mathf.Clamp(accel, -1, 1);
+        float thrustTorque = accel * torque;
+        WheelCollider.motorTorque = thrustTorque;
+
+        Quaternion quaternion;
+        Vector3 position;
+        WheelCollider.GetWorldPose(out position, out quaternion);
+        transform.position = position;
+        transform.rotation = quaternion;
+    }
+}
