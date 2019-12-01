8e2c54dc927b717592b87c245eb43381df7e48e8
thisisstreamliner <thisisstreamliner@gmail.com>
2019-12-01 09:25:35 +0200

    Added Waypoints

---
+++ Assets/Circuit.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Circuit : MonoBehaviour
+{
+    public GameObject[] waypoints;
+
+    private void OnDrawGizmos()
+    {
+        DrawGizmos(false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        DrawGizmos(true);
+    }
+
+    private void DrawGizmos(bool selected)
+    {
+        if (!selected || waypoints.Length <= 0) return;
+
+        Vector3 prev = waypoints[0].transform.position;
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            Vector3 next = waypoints[i].transform.position;
+            Gizmos.DrawLine(prev, next);
+            prev = next;
+        }
+        Gizmos.DrawLine(prev, waypoints[0].transform.position);
+    }
+
+}
